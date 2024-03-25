bbf3a1048be8839c683460ae8eb4e9342ffb1cbc
antonioln00 <antonioln00@gmail.com>
2024-03-25 13:54:11 -0300

    feat: criando componente de cabeçalho e rodapé

--- AngularBatizadoDoNovato/src/app/app.module.ts
+++ AngularBatizadoDoNovato/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProdutoIndexComponent } from './components/produto/produto-index/produto-index.component';
 import { ListarRegraImpostoComponent } from './components/regra-imposto/listar-regra-imposto/listar-regra-imposto.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProdutoIndexComponent,
-    ListarRegraImpostoComponent
+    ListarRegraImpostoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
